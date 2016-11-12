67fcc2b0bc5586be33424f5db2be4b84b79f4d16
lilyE <ola.soi.lily@gmail.com>
2016-11-12 10:33:10 -0400

    examen

---
+++ objetoEjemplo.js
@@ -0,0 +1,2 @@
+var tienda={producto:"azucar",precio:"10Bs",fechaCompra:"12/11/16"}
+console.log("producto: "+tienda.producto +"  precio: "+tienda.precio + "  fechaCompra: "+tienda.fechaCompra);
---
+++ tablaSuma.js
@@ -0,0 +1,23 @@
+var x=7;
+var a=0;
+function suma(numero){
+	for (var i = 0; i <= 10; i++) {
+		a=a+1;
+			
+		
+	 }
+	console.log("suma de numero 7 :");
+console.log("0 + 7 :",numero++);
+console.log("1 + 7 :",numero++);
+console.log("2 + 7 :",numero++);
+console.log("3 + 7 :",numero++);
+console.log("4 + 7 :",numero++);
+console.log("5 + 7 :",numero++);
+console.log("6 + 7 :",numero++);
+console.log("7 + 7 :",numero++);
+console.log("8 + 7 :",numero++);
+console.log("9 + 7 :",numero++); 
+console.log("10 + 7 :",numero++); 
+}
+suma(x);
+
